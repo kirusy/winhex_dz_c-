73607338486d7117711e0e96be83825ed82f9350
kirusy <ivancerepanov78620@gmail.com>
2024-12-04 22:18:21 +0500

    выводим в консоль поля, выделенные в таблице 1 жирным шрифтом

--- main.cpp
+++ main.cpp
@@ -20,11 +20,21 @@ struct head{
 };
 int main()
 {
-    vector<char> array1;
     ifstream file("D:\\Downloads\\File_record.dat", ios::binary);
     if(file.is_open())
     {
         cout << "open file" << endl;
+
+        file.seekg(0, ios::end);
+        int sizePoz = file.tellg();
+        vector<char> array1(sizePoz, 0);
+        file.seekg(0, ios::beg);
+        file.read(array1.data(), sizePoz);
+        head*p = reinterpret_cast<head*> (&array1[0]);
+        cout << p -> textString << endl;
+        cout << p -> ofsetAtributOne << endl;
+        cout << p -> fileNumber << endl;
+
     }
 
 
